49b4d2f71ef6deabd50f388f6f41f33753623cec
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-07-09 07:56:19 +0200

    Use words controller as root.

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ FlyingCarpet::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "words#index"
 
   # See how all your routes lay out with "rake routes"
 
