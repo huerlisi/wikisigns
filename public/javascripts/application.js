0abc3c803cf83fee2576137cd5cb61688928e726
Roman Simecek <roman.simecek@cyt.ch>
2011-01-21 18:04:35 +0100

    Input field has now an autofocus and the text is selected.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// Initialize behaviours
+function initializeBehaviours() {
+  addFocusTextFieldBehaviour();
+}
+
+function addFocusTextFieldBehaviour() {
+  $('#word_word').focus().select();
+}
+
+// Loads functions after DOM is ready
+$(document).ready(initializeBehaviours);
