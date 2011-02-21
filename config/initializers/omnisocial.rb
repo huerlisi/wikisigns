0e2d3e1c6f938ca7f4e16279158fea1206503e81
Roman Simecek <roman.simecek@cyt.ch>
2011-02-21 12:17:09 +0100

    Scope added.

--- config/initializers/omnisocial.rb
+++ config/initializers/omnisocial.rb
@@ -1,11 +1,11 @@
 Omnisocial.setup do |config|
   
   # ==> Twitter
-  config.twitter Settings.twitter.consumer_key, Settings.twitter.consumer_secret
+  config.twitter Settings.twitter.consumer_key, Settings.twitter.consumer_secret, :scope => ['publish_stream', 'email']
   
   # ==> Facebook
-  config.facebook Settings.facebook.consumer_key, Settings.facebook.consumer_secret, :scope => 'publish_stream'
-  
+  config.facebook Settings.facebook.consumer_key, Settings.facebook.consumer_secret, :scope => ['publish_stream', 'email']
+
   if Rails.env.production?
     
     # Configs for production mode go here
