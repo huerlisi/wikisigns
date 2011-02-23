1aaea1aa06ab231dd4798894cd583472761ce279
Roman Simecek <roman.simecek@cyt.ch>
2011-02-23 13:48:11 +0100

    Splitted the application js for normal view and facebook app view.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -98,29 +98,6 @@ function addSessionWordsBehaviour(){
   });
 }
 
-function displaySessionSmallWord(word_picture, text, id){
-  var share_link = generateShareLink(id);
-
-  word_picture = addSmallWordAttributes(word_picture);
-  $('#your-words').append('<div class="svg"><div class="svg-text">'+ text +'</div>'+ share_link +'</div>');
-  $('#your-words .svg:last-child').prepend(word_picture);
-  FB.XFBML.parse();
-}
-
-function generateShareLink(id) {
-  var link = "";
-
-  if($('.facebook-user').length>0){
-    link = '<fb:share-button href="http://' + window.location.hostname + '/words/' + id + '"></fb:share-button>';
-  }
-
-  if($('.twitter-user').length>0){
-    link = '<div class="social-media-links"><a class="twitter-share-button" href="http://twitter.com/share?url=http://' + window.location.hostname + '/words/' + id + '">Tweet</a></div>';
-  }
-
-  return link;
-}
-
 function addSmallWordAttributes(word_picture){
   word_picture[0].setAttribute('viewBox', '1 1 430 430');
   word_picture[0].setAttribute('width', '100');
---
+++ public/javascripts/facebook.js
@@ -0,0 +1,8 @@
+function displaySessionSmallWord(word_picture, text, id){
+  var share_link = '<fb:share-button href="http://' + window.location.hostname + '/words/' + id + '"></fb:share-button>';
+
+  word_picture = addSmallWordAttributes(word_picture);
+  $('#your-words').append('<div class="svg"><div class="svg-text">'+ text +'</div>'+ share_link +'</div>');
+  $('#your-words .svg:last-child').prepend(word_picture);
+  FB.XFBML.parse();
+}
