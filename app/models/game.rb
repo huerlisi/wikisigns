e2e85e36b63b62aa5bc63b67734acdff6002e2df
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-20 22:27:26 +0200

    Drop Game.send_score as we now use User.post_score_to_fb_wall.

--- app/models/game.rb
+++ app/models/game.rb
@@ -13,16 +13,4 @@ class Game < ActiveRecord::Base
 
     self.save
   end
-
-  private
-
-  def send_score
-    graph = Koala::Facebook::GraphAPI.new(self.user.access_token)
-    graph.put_wall_post(I18n.t('game.message.score.text', :score => self.score),
-                                                          { "name" => "WikiSigns.ch - The Game",
-                                                            "link" => "http://wikisigns.ch/game/new",
-                                                            "caption" => "{*actor*} spielte auf WikiSigns.ch das Wortratespiel.",
-                                                            "description" => "Probier es einfach auch mal aus.",
-                                                            "picture" => "http://wikisigns.ch/words/#{word.id}/svg.jpg"})
-  end
 end
