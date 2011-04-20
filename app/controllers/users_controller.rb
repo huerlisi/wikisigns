be8596307e71dcc689df6e20ee6109bff12d4f6d
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-20 23:13:36 +0200

    Move FB score posting to controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,18 @@ class UsersController < ApplicationController
   def daily_score
     @user = User.find(params[:id])
 
-    render :json => @user.post_score_to_fb_wall
-  end
+    unless @user.last_facebook_post && @user.last_facebook_post.today?
+      graph = Koala::Facebook::GraphAPI.new(@user.access_token)
+      graph.put_wall_post(I18n.t('game.message.score.text', :score => @user.daily_score),
+                                                            { "name" => "WikiSigns.ch - The Game",
+                                                              "link" => new_game_url,
+                                                              "caption" => "{*actor*} spielte auf WikiSigns.ch das Wortratespiel.",
+                                                              "description" => "Probier es einfach auch mal aus.",
+                                                              "picture" => svg_word_url(@user.games.last.word, :format => :jpg)})
+      @user.last_facebook_post = Date.today
+      @user.save
+    end
 
+    render :json => @user
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,6 @@
 class User < Omnisocial::User
   has_many :games
 
-  # Facebook
-  include ActionController::UrlWriter
-  def post_score_to_fb_wall
-    unless self.last_facebook_post && self.last_facebook_post.today?
-      graph = Koala::Facebook::GraphAPI.new(self.access_token)
-      graph.put_wall_post(I18n.t('game.message.score.text', :score => self.daily_score),
-                                                            { "name" => "WikiSigns.ch - The Game",
-                                                              "link" => new_game_url,
-                                                              "caption" => "{*actor*} spielte auf WikiSigns.ch das Wortratespiel.",
-                                                              "description" => "Probier es einfach auch mal aus.",
-                                                              "picture" => svg_word_url(self.games.last.word, :format => :jpg)})
-      self.last_facebook_post = Date.today
-      self.save
-    end
-  end
-
   def daily_score
     score = 0
     games.today.each { |g| score = score + g.score }
