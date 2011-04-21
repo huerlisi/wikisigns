0248bf20d5b9701cd507d7fee221b6a5f34a7103
Roman Simecek <roman.simecek@cyt.ch>
2011-04-21 13:35:43 +0200

    Only one highscore post per day disabled.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,15 @@ class User < Omnisocial::User
   has_many :games
 
   def post_score_to_fb_wall
-    unless self.last_facebook_post && self.last_facebook_post.today?
-      graph = Koala::Facebook::GraphAPI.new(self.access_token)
-      graph.put_wall_post(I18n.t('game.message.score.text', :score => self.daily_score),
-                                                            { "name" => "WikiSigns.ch - The Game",
-                                                              "link" => "http://wikisigns.ch/game/new",
-                                                              "caption" => "{*actor*} spielte auf WikiSigns.ch das Wortratespiel.",
-                                                              "description" => "Probier es einfach auch mal aus.",
-                                                              "picture" => "http://wikisigns.ch/words/#{self.games.last.word.id}/svg.jpg"})
-      self.last_facebook_post = Date.today
-      self.save
-    end
+    graph = Koala::Facebook::GraphAPI.new(self.access_token)
+    graph.put_wall_post(I18n.t('game.message.score.text', :score => self.daily_score),
+                                                          { "name" => "WikiSigns.ch - The Game",
+                                                            "link" => "http://wikisigns.ch/game/new",
+                                                            "caption" => "{*actor*} spielte auf WikiSigns.ch das Wortratespiel.",
+                                                            "description" => "Probier es einfach auch mal aus.",
+                                                            "picture" => "http://wikisigns.ch/words/#{self.games.last.word.id}/svg.jpg"})
+    self.last_facebook_post = Date.today
+    self.save
   end
 
   def daily_score
