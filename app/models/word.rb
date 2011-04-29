d51de434d8ff676c3b07796f64b5be8e152131c0
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-29 21:50:13 +0200

    Use more rails like scopes.

--- app/models/word.rb
+++ app/models/word.rb
@@ -15,15 +15,15 @@ class Word < ActiveRecord::Base
   ]
 
   # Scope for words without space in it.
-  scope :without_space, lambda { where("words.word NOT LIKE '% %'") }
+  scope :without_space, where("words.word NOT LIKE '% %'")
   # Scope for words with only ASCII chars without numbers
-  scope :without_special_chars, lambda { where("words.word REGEXP '^[a-zA-Z ]*$'") }
+  scope :without_special_chars, where("words.word REGEXP '^[a-zA-Z ]*$'")
   # Scope for minimal length of 3 chars
-  scope :minimal_length, lambda { where("LENGTH(words.word) > 2") }
+  scope :minimal_length, where("LENGTH(words.word) > 2")
 
   # The latest words, by default 12 entries.
   def self.latest(amount = 12)
-    self.all(:order => 'created_at DESC', :limit => amount)
+    order('created_at DESC').limit(amount)
   end
 
   # Gets a random word.
