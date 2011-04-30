cb864d220a1a25eb88b56f4b74f7b91b5ccb1d79
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-30 23:57:39 +0200

    Allow multiple FB posts per day.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,15 @@ class UsersController < ApplicationController
   def daily_score
     @user = User.find(params[:id])
 
-    unless @user.last_facebook_post && @user.last_facebook_post.today?
-      graph = Koala::Facebook::GraphAPI.new(@user.access_token)
-      graph.put_wall_post(I18n.t('game.message.score.text', :score => @user.daily_score),
-                                                            { "name" => "WikiSigns.ch - The Game",
-                                                              "link" => new_game_url,
-                                                              "caption" => "{*actor*} spielte auf WikiSigns.ch das Wortratespiel.",
-                                                              "description" => "Probier es einfach auch mal aus.",
-                                                              "picture" => svg_word_url(@user.games.last.word, :format => :jpg)})
-      @user.last_facebook_post = Date.today
-      @user.save
-    end
+    graph = Koala::Facebook::GraphAPI.new(@user.access_token)
+    graph.put_wall_post(I18n.t('game.message.score.text', :score => @user.daily_score),
+                                                          { "name" => "WikiSigns.ch - The Game",
+                                                            "link" => new_game_url,
+                                                            "caption" => "{*actor*} spielte auf WikiSigns.ch das Wortratespiel.",
+                                                            "description" => "Probier es einfach auch mal aus.",
+                                                            "picture" => svg_word_url(@user.games.last.word, :format => :jpg)})
+    @user.last_facebook_post = Date.today
+    @user.save
 
     render :json => @user
   end
