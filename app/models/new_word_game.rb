01360c5d9de2da716d068db54720af2cff451249
Roman Simecek <roman.simecek@cyt.ch>
2011-05-02 11:55:50 +0200

    Calculate score for NewWordGame implemented.

--- app/models/new_word_game.rb
+++ app/models/new_word_game.rb
@@ -1,13 +1,19 @@
 class NewWordGame < Game
-  def check_game
-    check_inserted_word
-    calculate_score
+
+  # Overall score
+  #
+  # PI is the only allowed constant in these calculations!!!
+  def calculate_score
+    unless Word.where(:word => self.input).exists?
+      self.word = Word.create(:word => self.input, :user => self.user)
+      self.won = true
+    end
   end
 
   private
 
   def check_inserted_word
-    unless Word.find_by_word(self.input)
+    unless Word.where(:word => self.input).exists?
       self.word = Word.create(:word => self.input, :user => self.user)
       self.won = true
     end
