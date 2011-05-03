af875bc3dea991a0608fde4fc886fd8dadc6e544
Roman Simecek <roman.simecek@cyt.ch>
2011-05-03 15:11:11 +0200

    Score for a new game.

--- app/models/new_word_game.rb
+++ app/models/new_word_game.rb
@@ -6,6 +6,7 @@ class NewWordGame < Game
   def calculate_score
     self.word = Word.find_or_initialize_by_word(self.input)
     self.won = self.word.new_record?
+    self.score = word_factor * length_factor * random_factor * profile_factor
   end
 
 end
