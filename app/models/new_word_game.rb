7ea4b70148a93c4a804deb9db668f6eb554d7336
Roman Simecek <roman.simecek@cyt.ch>
2011-05-03 15:19:39 +0200

    Score calculation for new word game hot fixed.

--- app/models/new_word_game.rb
+++ app/models/new_word_game.rb
@@ -4,9 +4,14 @@ class NewWordGame < Game
   #
   # PI is the only allowed constant in these calculations!!!
   def calculate_score
+    self.score = word_factor * random_factor * profile_factor * new_word_factor
     self.word = Word.find_or_initialize_by_word(self.input)
     self.won = self.word.new_record?
-    self.score = word_factor * length_factor * random_factor * profile_factor
   end
 
+  private
+
+  def new_word_factor
+    Math::PI*Math::PI
+  end
 end
