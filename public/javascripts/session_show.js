bbee4f3bf882a8cf164f48f91e8d060e43740495
Roman Simecek <roman.simecek@cyt.ch>
2011-05-11 14:08:30 +0200

    Code comments added.

--- public/javascripts/session_show.js
+++ public/javascripts/session_show.js
@@ -2,18 +2,19 @@ var session_viewer;
 var session_viewer_start;
 var picture_to_show = 1;
 
-// Start des Session viewers
+// Start the session viewers show.
 function startSessionViewer() {
   clearSessionViewerIntervals();
   session_viewer_start = setInterval('initializeShowSessionViewer()', 30000)
 }
 
+// Continue with the session viewer show.
 function initializeShowSessionViewer() {
   clearSessionViewerIntervals();
   session_viewer = setInterval('showSessionViewer()', 1000);
 }
 
-// Bild anzeigen als Session viewer.
+// Show a picture as session viewer
 function showSessionViewer() {
   if($('#your-words .one-word').length < picture_to_show){
     picture_to_show = 1;
@@ -22,6 +23,7 @@ function showSessionViewer() {
   picture_to_show++;
 }
 
+// Clears all intervals that are used for the session viewer.
 function clearSessionViewerIntervals() {
   clearInterval(session_viewer_start);
   clearInterval(session_viewer);
