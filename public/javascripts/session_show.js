7498f07b63307e51aae5ddab4f54b3d54c5cc455
Roman Simecek <roman.simecek@cyt.ch>
2011-05-12 11:12:08 +0200

    Timeout for session show set.

--- public/javascripts/session_show.js
+++ public/javascripts/session_show.js
@@ -1,6 +1,7 @@
 var session_viewer;
 var session_viewer_start;
 var picture_to_show = 1;
+var session_viewer_timeout;
 
 // Start the session viewers show.
 function startSessionViewer() {
@@ -12,6 +13,9 @@ function startSessionViewer() {
 function initializeShowSessionViewer() {
   clearSessionViewerIntervals();
   session_viewer = setInterval('showSessionViewer()', 1000);
+  $('#start-session-show').hide(0, function(){
+    $('#pause-session-show').show();
+  })
 }
 
 // Starts the session viewer.
@@ -35,17 +39,20 @@ function pauseSessionViewer() {
 
 // Show a picture as session viewer
 function showSessionViewer() {
+  clearSessionViewerIntervals();
   if($('#your-words .one-word').length < picture_to_show){
     picture_to_show = 1;
   }
   $('#your-words .one-word:nth-child(' + picture_to_show + ')').trigger('click');
   picture_to_show++;
+  session_viewer_timeout = setTimeout('showSessionViewer()', 2000);
 }
 
 // Clears all intervals that are used for the session viewer.
 function clearSessionViewerIntervals() {
   clearInterval(session_viewer_start);
   clearInterval(session_viewer);
+  clearTimeout(session_viewer_timeout);
 }
 
 function shareSessionLink() {
