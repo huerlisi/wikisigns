1944493554253db9b8f5dafe49c0c3592d08fb14
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-06-27 23:00:03 +0200

    Drop global word_position; rename variables in drawWord.

--- public/javascripts/show_word.js
+++ public/javascripts/show_word.js
@@ -1,7 +1,6 @@
 var letter_speed = 314 * 3.14;
 var show_word_interval;
 var draw_word_interval;
-var word_position;
 
 // Show Word
 // =========
@@ -24,18 +23,18 @@ function startShowWord(word) {
   draw_word_interval = setTimeout(function(){drawWord(word, 0)}, letter_speed);
 }
 
-function drawWord(original_word, word_position) {
-  if (word_position < original_word.length) {
-    var word = original_word.slice(0, word_position + 1);
+function drawWord(word, position) {
+  if (position < word.length) {
+    var part = word.slice(0, position + 1);
 
     // Main sign
-    updateTitle(word);
-    updateWord(word);
+    updateTitle(part);
+    updateWord(part);
 
-    draw_word_interval = setTimeout(function(){drawWord(original_word, word_position + 1)}, letter_speed);
+    // Call self again, incrementing position
+    draw_word_interval = setTimeout(function(){drawWord(word, position + 1)}, letter_speed);
   } else {
     clearInterval(show_word_interval);
-    clearInterval(draw_word_interval);
     setTimeout(function(){
       session_viewer_timeout = setTimeout('showSessionViewer()', letter_speed);
     }, letter_speed*2);
