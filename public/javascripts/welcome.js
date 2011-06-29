a7423ffa1606bfe5536fed8b6eeeef1a8a6a0d63
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-06-29 23:18:37 +0200

    Factor out anonymous callback functions in setWelcomeMode.

--- public/javascripts/welcome.js
+++ public/javascripts/welcome.js
@@ -1,38 +1,41 @@
 // Welcome Mode
 // ============
 
+// Callbacks
+function finished_welcome() {
+  $('#left-container').hide(125, function(){
+    window.location = '/words/new';
+  });
+}
+
+function draw_welcome_title(text, callback) {
+  var colored_text = $(drawColoredWord(text));
+  var last_letter = colored_text.filter('span:last');
+
+  last_letter.css('position', 'relative');
+  last_letter.css('left', '3000px');
+  last_letter.css('top', '2000px');
+  last_letter.css('z-index', '1');
+  $('#title').html(colored_text);
+
+  last_letter.animate({
+                        top: '0px',
+                        left: '0px'
+                      },
+                      250,
+                      function(){
+                        if(typeof(callback) != "undefined"){
+                          callback();
+                        }
+                      });
+}
+
 // Loads the guessing game on the root page.
 // It starts with the alphabet.
 function setWelcomeMode() {
   var alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
-  startShowWord(alphabet,
-                function(){
-                  $('#left-container').hide(125, function(){
-                    window.location = '/words/new';
-                  });
-                },
-                function(text, callback){
-                  var colored_text = $(drawColoredWord(text));
-                  var last_letter = colored_text.filter('span:last');
-
-                  last_letter.css('position', 'relative');
-                  last_letter.css('left', '3000px');
-                  last_letter.css('top', '2000px');
-                  last_letter.css('z-index', '1');
-                  $('#title').html(colored_text);
-
-                  last_letter.animate({
-                                        top: '0px',
-                                        left: '0px'
-                                      },
-                                      250,
-                                      function(){
-                                        if(typeof(callback) != "undefined"){
-                                          callback();
-                                        }
-                                      });
-                });
+  startShowWord(alphabet, finished_welcome, draw_welcome_title);
 }
 
 $(document).ready(initializeBehaviours);
