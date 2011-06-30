948878c7cb19c39f85ad3f0f595dbdafafdbc61c
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-06-30 22:20:57 +0200

    Implement stopShowWord.

--- public/javascripts/show_word.js
+++ public/javascripts/show_word.js
@@ -1,5 +1,5 @@
 var letter_speed = 314 * 3.14;
-var draw_word_interval;
+var draw_word_timeout;
 
 // Show Word
 // =========
@@ -19,9 +19,13 @@ function showAsBigWord(text) {
 }
 
 function startShowWord(word, after_finish, draw_title) {
-  draw_word_interval = setTimeout(function(){
+  draw_word_timeout = setTimeout(function(){
     drawWord(word, 0, after_finish, draw_title)
   }, letter_speed);
+}
+
+function stopShowWord() {
+  clearInterval(draw_word_timeout);
 }
 
 // Incremential show a word
@@ -34,10 +38,10 @@ function drawWord(word, position, after_finish, draw_title) {
     if(typeof(draw_title) == "undefined") {
       updateTitle(part);
       // Call self again, incrementing position
-      draw_word_interval = setTimeout(function(){drawWord(word, position + 1, after_finish, draw_title)}, letter_speed);
+      draw_word_timeout = setTimeout(function(){drawWord(word, position + 1, after_finish, draw_title)}, letter_speed);
     }else{
       draw_title(part, function(){
-        draw_word_interval = setTimeout(function(){drawWord(word, position + 1, after_finish, draw_title)}, letter_speed);
+        draw_word_timeout = setTimeout(function(){drawWord(word, position + 1, after_finish, draw_title)}, letter_speed);
       });
     }
   } else {
