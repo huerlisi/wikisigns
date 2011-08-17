c2edb19d3834912fb8fc6f3d31f312668c666688
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-08-17 10:53:32 +0200

    Unify staging deploy config with other projects.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,9 @@
+# Deployment
 set :rails_env, 'staging'
 set :branch, "master"
 
-set :deploy_to, '/srv/cyt.ch/wikisigns'
-role :web, "test.wikisigns"                          # Your HTTP server, Apache/etc
-role :app, "test.wikisigns"                          # This may be the same as your `Web` server
-role :db,  "test.wikisigns", :primary => true        # This is where Rails migrations will run
+set :deploy_to, "/srv/cyt.ch/#{application}"
+role :web, "test.#{application}"                          # Your HTTP server, Apache/etc
+role :app, "test.#{application}"                          # This may be the same as your `Web` server
+role :db,  "test.#{application}", :primary => true        # This is where Rails migrations will run
 
