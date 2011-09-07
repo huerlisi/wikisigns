a161c128421413128c6ad7ea588f18c8cf9c4a0b
Roman Simecek <roman.simecek@cyt.ch>
2011-09-07 15:14:39 +0200

    Background lines of the cube are now also drawn in the sign.

--- public/javascripts/raphael/word.js
+++ public/javascripts/raphael/word.js
@@ -305,6 +305,25 @@ cube[10] = new Array(2);
 cube[10][0] = 3;
 cube[10][1] = 0;
 
+var shadow_cube0 = new Array();
+shadow_cube0[0] = new Array()
+shadow_cube0[0][0] = 0;
+shadow_cube0[0][1] = 3;
+shadow_cube0[1] = new Array()
+shadow_cube0[1][0] = 1;
+shadow_cube0[1][1] = 2;
+shadow_cube0[2] = new Array()
+shadow_cube0[2][0] = 1;
+shadow_cube0[2][1] = 0;
+
+var shadow_cube1 = new Array();
+shadow_cube1[0] = new Array()
+shadow_cube1[0][0] = 1;
+shadow_cube1[0][1] = 2;
+shadow_cube1[1] = new Array()
+shadow_cube1[1][0] = 3;
+shadow_cube1[1][1] = 2;
+
 // Draws a word as an image.
 function drawWordAsImage(element, input_word, size) {
   // Guard
@@ -391,18 +410,42 @@ function drawWordAsImage(element, input_word, size) {
   if(scale == 1){
     for(var z = 0; z < cube.length; z++){
       if(cube[z + 1] != null){
-        var from_x = margin + circle_dimension/2 + cube[z][0] * circle_dimension + space * cube[z][0];
+        /**var from_x = margin + circle_dimension/2 + cube[z][0] * circle_dimension + space * cube[z][0];
         var from_y = margin + circle_dimension/2 + cube[z][1] * circle_dimension + space * cube[z][1];
         var to_x = margin + circle_dimension/2 + cube[z + 1][0] * circle_dimension + space * cube[z + 1][0];
         var to_y = margin + circle_dimension/2 + cube[z + 1][1] * circle_dimension + space * cube[z + 1][1];
         var line = paper.path("M" + from_x + " " + from_y + "L" + to_x + " " + to_y);
 
+        line.attr({opacity: 0.5});**/
+        line = drawCubeLine(paper, margin, circle_dimension, space, cube[z][0], cube[z][1], cube[z + 1][0], cube[z + 1][1]);
         line.attr({opacity: 0.5});
+      }
+    }
+    for(var z = 0; z < shadow_cube0.length; z++) {
+      if(shadow_cube0[z + 1] != null) {
+        line = drawCubeLine(paper, margin, circle_dimension, space, shadow_cube0[z][0], shadow_cube0[z][1], shadow_cube0[z + 1][0], shadow_cube0[z + 1][1]);
+        line.attr({opacity: 0.2});
+      }
+    }
+    for(var z = 0; z < shadow_cube1.length; z++) {
+      if(shadow_cube1[z + 1] != null) {
+        line = drawCubeLine(paper, margin, circle_dimension, space, shadow_cube1[z][0], shadow_cube1[z][1], shadow_cube1[z + 1][0], shadow_cube1[z + 1][1]);
+        line.attr({opacity: 0.2});
       }
     }
   }
 
   paper.setSize(size, size);
+}
+
+function drawCubeLine(paper, margin, circle_dimension, space, start_x, start_y, end_x, end_y) {
+  var from_x = margin + circle_dimension/2 + start_x * circle_dimension + space * start_x;
+  var from_y = margin + circle_dimension/2 + start_y * circle_dimension + space * start_y;
+  var to_x = margin + circle_dimension/2 + end_x * circle_dimension + space * end_x;
+  var to_y = margin + circle_dimension/2 + end_y * circle_dimension + space * end_y;
+  var line = paper.path("M" + from_x + " " + from_y + "L" + to_x + " " + to_y);
+
+  return line;  
 }
 
 // Creates a colored word.
